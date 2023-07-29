012cc1ad8c9df8b627f8c411c21b8eaee420dc50
Guilherme Tolio dos Santos <105080185+GuilhermeTolio@users.noreply.github.com>
2023-07-28 21:45:26 -0300

    Add files via upload

---
+++ aula1/src/exemplo1.java
@@ -0,0 +1,12 @@
+public class exemplo1 {
+    public static void main(String[] args) {
+        System.out.println("Domingo");
+        System.out.println("Segunda");
+        System.out.println("Terça");
+        System.out.println("Quarta");
+        System.out.println("Quinta");
+        System.out.println("Sexta");
+        System.out.println("Sabado");
+        System.exit(0);
+    }
+}
---
+++ aula1/src/exemplo2.java
@@ -0,0 +1,24 @@
+public class exemplo2 {
+    public static void main(String[] args) {
+        System.out.println("Churasco");
+        System.out.println("Bom");
+        System.out.println("Chimarrão");
+        System.out.println("Fandango");
+        System.out.println("Trago");
+        System.out.println("E");
+        System.out.println("Mulher");
+        System.out.println("É");
+        System.out.println("Isso");
+        System.out.println("Que");
+        System.out.println("O");
+        System.out.println("Veio");
+        System.out.println("Gosta");
+        System.out.println("É");
+        System.out.println("Isso");
+        System.out.println("Que");
+        System.out.println("O");
+        System.out.println("Veio");
+        System.out.println("Quer");
+        System.exit(0);
+    }
+}
